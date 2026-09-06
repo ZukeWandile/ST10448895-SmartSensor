7b77b11a89475e5ab9f1124bd6b779bd1b6881dc
Wandile <ST10448895@imconnect.edu.za>
2026-09-06 22:34:47 +0200

    Expose FirebaseAuthResponse and add JSON mappings
    Make FirebaseAuthResponse public, add System.Text.Json.Serialization and strongly-typed properties with JsonPropertyName attributes for idToken, email, localId and refreshToken.

--- sensorX/Services/FirebaseAuthResponse.cs
+++ sensorX/Services/FirebaseAuthResponse.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace sensorX.Services
 {
-    class FirebaseAuthResponse
+    // The raw data Firebase returns after a successful sign-in OR sign-up call.
+    public class FirebaseAuthResponse
     {
+        [JsonPropertyName("idToken")]
+        public string IdToken { get; set; } = "";
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; } = "";
+
+        [JsonPropertyName("localId")]
+        public string LocalId { get; set; } = "";
+
+        [JsonPropertyName("refreshToken")]
+        public string RefreshToken { get; set; } = "";
     }
 }
