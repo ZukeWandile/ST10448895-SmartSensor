379a7da8e094082603dd3058da424f49ed474fe5
Wandile <ST10448895@imconnect.edu.za>
2026-09-06 22:31:08 +0200

    Add Firebase auth service stubs
    Introduce four new service stubs under sensorX/Services: FirebaseAuthResponse.cs, FirebaseAuthResult.cs, FirebaseAuthService.cs, and FirebaseSettings.cs (empty classes in sensorX.Services).

--- sensorX/obj/Debug/net9.0-windows/sensorX.AssemblyInfo.cs
+++ sensorX/obj/Debug/net9.0-windows/sensorX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sensorX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b588b6d9931c4a3665197772dad396366e981cd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91d9a6d483b25eb528ff6129e54c30671d6bd66")]
 [assembly: System.Reflection.AssemblyProductAttribute("sensorX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sensorX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
